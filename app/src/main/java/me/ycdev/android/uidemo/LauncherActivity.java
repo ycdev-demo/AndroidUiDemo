5d47835d6f96ab041f538d4e76b033332589ce3a
Yongce Tu <yongce.tu@gmail.com>
2015-06-15 19:24:25 +0800

    change: Update gradle version

--- app/src/main/java/me/ycdev/android/uidemo/LauncherActivity.java
+++ app/src/main/java/me/ycdev/android/uidemo/LauncherActivity.java
@@ -4,11 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import me.ycdev.android.uidemo.base.GridEntriesActivity;
 import me.ycdev.android.uidemo.drawables.DrawablesActivity;
+import me.ycdev.android.uidemo.utils.AppLogger;
 
 public class LauncherActivity extends GridEntriesActivity {
+    private static final String TAG = "LauncherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppLogger.d(TAG, "LauncherActivity#onCreate()");
+    }
+
     @Override
     protected boolean needLoadIntentsAsync() {
         return true;
